c3170f3aca0ec8024e99d67aca182229f84359df
Jasmine Shaik <shaikjasmine2115@gmail.com>
2026-07-17 21:04:49 +0530

    GH-finder_UI

--- Github-finder/src/App.tsx
+++ Github-finder/src/App.tsx
@@ -1,7 +1,10 @@
+import UserSearch from "../components/UserSearch";
+
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Github Finder</h1>
+      <UserSearch />
     </div>
   );
 }
--- Github-finder/src/main.tsx
+++ Github-finder/src/main.tsx
@@ -7,7 +7,9 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </QueryClientProvider>
 )
